2dfaaf24c2e402444fbdce41a69791db5b4b1cf8
Gabriella <117856236+carlosthepineapple@users.noreply.github.com>
2025-03-06 18:11:24 -0800

    tds lstm

--- emg2qwerty/modules.py
+++ emg2qwerty/modules.py
@@ -279,7 +279,55 @@ class TDSConvEncoder(nn.Module):
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         return self.tds_conv_blocks(inputs)  # (T, N, num_features)
     
+
+
+class TDSLSTMEncoder(nn.Module):
+
+    def __init__(
+        self,
+        num_features: int,
+        lstm_hidden_size: int = 128,
+        num_lstm_layers: int = 4,
+    ) -> None:
+        super().__init__()
+
+        self.lstm_layers = nn.LSTM(
+            input_size = num_features,
+            hidden_size = lstm_hidden_size,
+            num_layers = num_lstm_layers,
+            batch_first = False,
+            bidirectional = True,
+        )
+        self.fc_block = TDSFullyConnectedBlock(lstm_hidden_size * 2)
+        self.out_layer = nn.Linear(lstm_hidden_size * 2, num_features)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x, _ = self.lstm_layers(inputs)
+        x = self.fc_block(x)
+        x = self.out_layer(x)
+
+        return x
+
+        assert len(block_channels) > 0
+        tds_conv_blocks: list[nn.Module] = []
+        for channels in block_channels:
+            assert (
+                num_features % channels == 0
+            ), "block_channels must evenly divide num_features"
+            tds_conv_blocks.extend(
+                [
+                    TDSConv2dBlock(channels, num_features // channels, kernel_width),
+                    TDSFullyConnectedBlock(num_features),
+                ]
+            )
+        self.tds_conv_blocks = nn.Sequential(*tds_conv_blocks)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return self.tds_conv_blocks(inputs)  # (T, N, num_features)
     
+
+
+
 class SimpleCNN2dBlock(nn.Module):
     """A basic convolutional neural network for processing EMG spectrograms."""
     
