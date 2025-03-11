ac06ad1d5f5c50228ead2e0daca5b4c6027c2cf4
Gabriella <117856236+carlosthepineapple@users.noreply.github.com>
2025-03-10 21:12:44 -0700

    ok

--- emg2qwerty/transforms.py
+++ emg2qwerty/transforms.py
@@ -277,7 +277,7 @@ class RandomCrop:
         if T < min_valid_crop:
             pad_amount = min_valid_crop - T
             tensor = torch.nn.functional.pad(tensor, (0, 0, 0, 0, 0, pad_amount))  # Pad time dimension
-
+        
         crop_size = np.random.randint(min_valid_crop, min(self.max_crop_size, T) + 1)
         start_idx = np.random.randint(0, T - crop_size + 1)
         cropped_tensor = tensor[start_idx : start_idx + crop_size]
